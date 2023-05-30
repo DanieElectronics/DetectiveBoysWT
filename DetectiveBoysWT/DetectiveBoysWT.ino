10e8007bf9b655c38fe5031dce6bd4301cabebbe
DanieElectronics <flyner96@gmail.com>
2023-05-30 21:04:21 +0200

    Updated PCB (v1.2)
    Added a flexPCB connector on the edge of the board connected to the ICSP pins, making easier to program the ATMEGA328P.
    The battery terminals footprint is now only on one side of the board, preventing accidental short-circuit on the bottom side.
    Moved some components to allow more space for the LiPo battery.
    Changed the values of components C7,C9 R5,R6, adjusting the values for filtering the audio output.

--- DetectiveBoysWT/DetectiveBoysWT.ino
+++ DetectiveBoysWT/DetectiveBoysWT.ino
@@ -1,34 +1,11 @@
+//// Detective Boys Walkie Talkie ////
 
-/* RF24 Audio Library TMRh20 2014
-
-This sketch is intended to demonstrate the most basic functionality of the audio library.
-
-Requirements:
-2 Arduinos (Uno,Nano,Mega, etc supported)
-2 NRF24LO1 Radio Modules
-1 or more input devices (microphone, ipod, etc)
-1 or more output devices (speaker, amplifier, etc)
-1 or more external buttons or switches to control recording/volume/etc.
-
-Setup:
-1. Change the CE,CS pins below to match your chosen pins (I use 7,8 on 328 boards, and 48,49 on Mega boards)
-2. Connect buttons as desired to the button pins listed below.
-2. Upload this sketch to two or more devices
-3. Use external buttons to control all devices
-
-Default Pin Selections:
-Speaker: pins 9,10 on UNO, Nano,  pins 11,12 on Mega 2560
-Input/Microphone: Analog pin A0 on all boards
-
-Transmit button: Pin A1
-VolumeUp button: Pin A2
-VolumeDn button: Pin A3
-Remote Transmission: Pin A4
-
-Note: See http://arduino.cc/en/Tutorial/InputPullupSerial for info on how to wire the buttons to the pins
-Note: Pin selections can be overridden by modifying the userConfig.h file included with the library
+/* 
+  This software uses RF24Audio library to send audio data between NRF24L01 radio modules
+  This code runs on an ATMEGA328P microcontroller at 3.3v & 8MHZ
+  Use Arduino Pro Mini 3.3v 8MHz at board selection in Arduino IDE
+  You must burn the bootloader before uploading the code
 */
-
 
 #include "RF24.h"
 #include <SPI.h>
@@ -36,23 +13,16 @@ Note: Pin selections can be overridden by modifying the userConfig.h file includ
 
 RF24 radio(7,8);                // Set radio up using pins 7 (CE) 8 (CS)
 RF24Audio rfAudio(radio,1);     // Set up the audio using the radio, and set to radio number 0.
-                                // Setting the radio number is only important if one-to-one communication is desired
-                                // in a multi-node radio group. See the privateBroadcast() function.
 
 void setup() {          
 
   rfAudio.begin();              // Initialize the library.
-  rfAudio.setVolume(4);
+  rfAudio.setVolume(4);         // Set volume at 50% (4/7)
 
 }
 
 void loop() {
-  
   rfAudio.handleButtons();
-  // Audio playback is all managed internally.
-  // In this example, the radio is controlled by external buttons, so there is nothing to do here
-  // except handle the buttons.
-  
 }
 
 
